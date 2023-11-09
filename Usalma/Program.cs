7f43f5f1f0c0333136d871f59c60ef306c231b1e
Medeni Aba <medeniabax@gmail.com>
2023-11-09 14:22:12 +0300

    Proje dosyası ekle.

---
+++ Usalma/Program.cs
@@ -0,0 +1,15 @@
+﻿int sayi;
+int üs;
+int toplam = 1; 
+Console.WriteLine("Hesaplanacak sayıyı giriniz : ");
+sayi = int.Parse(Console.ReadLine());
+Console.WriteLine("Hesaplanacak üs degerini giriniz :");
+üs = int.Parse(Console.ReadLine());
+
+for ( int i = 1; i <= üs; i++)
+{
+     toplam *= sayi;  
+
+}
+Console.WriteLine(toplam);
+Console.ReadKey();
